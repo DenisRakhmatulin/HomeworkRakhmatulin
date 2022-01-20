ead596fa6b82b0049fa2f89bfa63b3ef113ef803
DenisRakhmatulin <sardavis82@gmail.com>
2022-01-20 21:46:56 +1200

    Task 2 complete. Created: new layout, new Java Class. Update Manifest.

---
+++ app/src/main/java/com/example/homeworkrakhmatulin/Lesson01Task02Activity.java
@@ -0,0 +1,13 @@
+package com.example.homeworkrakhmatulin;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Lesson01Task02Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lesson01_task02_act_main);
+    }
+}
